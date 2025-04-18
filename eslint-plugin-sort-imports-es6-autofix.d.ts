8fa12318cc6f7020593b584d45ad557d587cb6b7
Diego Lamarão <diego.lamarao92@gmail.com>
2025-04-18 17:47:36 -0300

    update code structure

---
+++ eslint-plugin-sort-imports-es6-autofix.d.ts
@@ -0,0 +1,4 @@
+declare module 'eslint-plugin-sort-imports-es6-autofix' {
+    const plugin: any;
+    export default plugin;
+  }
--- eslint.config.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@taskchain/eslint-config' {
-    import { Linter } from 'eslint';
-
-    const config: Linter.Config[];
-
-    export default config;
-}
-    
